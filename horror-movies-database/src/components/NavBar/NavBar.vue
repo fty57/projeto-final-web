55f6670c1773c8151d0c9187e8d376aa40a3fd23
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-09-02 11:06:06 -0300

    Adição do Bootstrap
    - Bootstrap
    - BootstrapVue
    - Bootstrap Icons

---
+++ horror-movies-database/src/components/NavBar/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h5>THE SILVER SCREAM</h5>
+      </div>
+      <div class="col">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            aria-label="Pesquisar"
+            aria-describedby="button-addon2"
+          />
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+          >
+            <i class="bi bi-search"></i>
+          </button>
+        </div>
+      </div>
+      <div class="col">TEXTO1</div>
+      <div class="col">TEXTO2</div>
+      <div class="col">
+        <img
+          src=".././../img/vanallope.jpg"
+          class="rounded mx-auto d-block"
+          alt="Vanallope"
+          width="50px"
+          height="50px"
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ horror-movies-database/src/views/HomeView/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBarVue from "@/components/NavBar/NavBar.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+</script>
+
+<template>
+  <div class="homeMain">
+    <NavBarVue></NavBarVue>
+  </div>
+</template>
+
+<style scoped>
+.homeMain {
+  padding: 2.5%;
+}
+</style>
