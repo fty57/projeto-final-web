e04586aa9e023c2133d915f01ef4591d108d46fe
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-09-05 11:21:22 -0300

    Update HomeView.vue

--- horror-movies-database/src/views/HomeView/HomeView.vue
+++ horror-movies-database/src/views/HomeView/HomeView.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
 import NavBarVue from "@/components/NavBar/NavBar.vue";
+import MovieCard from "@/components/MovieCard/MovieCard.vue";
+import MovieGroup from "@/components/MovieGroup/MovieGroup.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 </script>
 
 <template>
-  <div class="homeMain">
+  <div class="HomeMain">
     <NavBarVue></NavBarVue>
+    <div style="height: 500px"></div>
+  </div>
+
+  <div class="HomeOffMain">
+    <div class="HomeBody">
+      <MovieGroup></MovieGroup>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.homeMain {
+.HomeMain {
   padding: 2.5%;
+  background-color: #c9c9c9;
+}
+.HomeBody {
+  background-color: #141810;
 }
 </style>
